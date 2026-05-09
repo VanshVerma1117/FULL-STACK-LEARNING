7ca8207ff85d59782551883143e54e8a8a37d3e6
Vansh Verma <vanshverma1117@gmail.com>
2026-05-09 19:55:09 +0530

    Intro to JS

---
+++ JAVASCRIPT/Intro to JS and installing Node/new.js
@@ -0,0 +1,3 @@
+console.log("hey this is a console text with console.log()")
+console.log("2nd console text ")
+console.log("3rd console text how is it looking ")
---
+++ JAVASCRIPT/Intro to JS and installing Node/script.js
@@ -0,0 +1,20 @@
+alert("welcome to dark web")
+
+console.log("hey this is a console text with console.log()")
+console.log("2nd console text ")
+console.log("3rd console text how is it looking ")
+
+var ab = prompt("Your number")
+console.log("Your number is " + ab )
+
+var istrue = confirm("your computer is about to be blast , Do you want to save it?")
+
+if (istrue){
+    console.log("your computer is blasting in 3")
+}
+
+else {
+    console.log("your computer is saved")
+}
+
+document.title = "Trial to change title"
