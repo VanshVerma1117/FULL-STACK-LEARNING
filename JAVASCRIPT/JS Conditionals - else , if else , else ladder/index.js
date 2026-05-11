31eb7daee5b881b8355ba44c4de2c39a527b049e
Vansh Verma <vanshverma1117@gmail.com>
2026-05-11 23:35:38 +0530

    Conditional statements

---
+++ JAVASCRIPT/JS Conditionals - else , if else , else ladder/index.js
@@ -0,0 +1,34 @@
+console.log("hello we will be learning conditional statements today") 
+
+let age = 0;
+// let grace = 1;
+
+// age += grace;
+// console.log(age)
+// console.log(age - grace)
+// console.log(age * grace)
+// console.log(age ** grace)
+// console.log(age / grace)
+// console.log(age % grace)
+// console.log(age ++ grace)
+// console.log(age -- grace)
+
+// if ((age + grace)>=18)
+//    if (age==18 )  
+   if (age==18) {
+    console.log("you can drive")
+} 
+else if (age==0){
+    console.log("Are you sure u can drive?")
+}
+else {
+    console.log("you cannot drive")
+}   
+
+let a = 7
+let b = 8
+
+let c = a>b ?(a-b):(b-a); //using ternary operator = ?
+{
+    console.log(c)
+}
