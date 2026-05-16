d67e7d408c4799798e83a705820ddd5a1e8dfab6
Vansh Verma <vanshverma1117@gmail.com>
2026-05-16 14:01:02 +0530

    Quiz on objects in JS

---
+++ JAVASCRIPT/QUIZ ON OBJECTS in JS/script.js
@@ -0,0 +1,21 @@
+let StudentA = {
+    name : "Fenix" ,
+    "phone number" : 91243387244 ,
+    marks : 89
+    
+}
+let StudentB = {
+    name : "Illy" ,
+    "phone number" : 91243387244 ,
+    marks : 68
+    
+}
+let StudentC = {
+    name : "Aspect" ,
+    "phone number" : 91243387244 ,
+    marks : 82 
+}
+
+console.log(StudentA);
+console.log(StudentB);
+console.log(StudentC);
