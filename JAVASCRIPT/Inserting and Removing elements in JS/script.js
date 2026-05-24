bb7cce00ca267224e2f4e42f5ced29f6ecd21a78
Vansh Verma <vanshverma1117@gmail.com>
2026-05-24 07:49:35 +0530

    Inserting and Removing elements in JS

---
+++ JAVASCRIPT/Inserting and Removing elements in JS/script.js
@@ -0,0 +1,22 @@
+// all commands to be run in browser console
+
+document.querySelector(".box")
+document.querySelector(".box").innerHTML
+document.querySelector(".container").innerHTML
+document.querySelector(".container").outerHTML
+document.querySelector(".container").tagName
+document.querySelector(".container").nodeName
+document.querySelector(".container").textContent
+// document.querySelector(".container").hidden
+// document.querySelector(".container").hidden = true
+document.querySelector(".box").innerHTML = "Hey i'm changing inner html through JS"
+document.querySelector(".box").hasAttribute("style")
+document.querySelector(".box").getAttribute("style")
+document.querySelector(".box").setAttribute("style" , "display: inline")
+document.querySelector(".box").removeAttribute("style")
+document.querySelector(".box").dataset
+document.querySelector(".container").classList
+document.querySelector(".container").className
+document.querySelector(".container").classList.add(".Line")
+document.querySelector(".container").classList.remove(".Line")
+document.querySelector(".container").classList.toggle("red")
