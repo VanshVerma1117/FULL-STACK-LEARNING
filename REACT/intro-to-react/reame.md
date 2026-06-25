7f230de457d5e3592a7b52853c2bf5b970442263
Vansh Verma <vanshverma1117@gmail.com>
2026-06-25 11:43:55 +0530

    Creating first react app

---
+++ REACT/intro-to-react/reame.md
@@ -0,0 +1,5 @@
+# Why React?
+- we can use states which means that once we change the variable,it change across the page.
+- we can split our app into multiple components and reuse those components.
+- React uses a virtual DOM to effeciently update the UI which is better than updating content using DOM Manipulation.
+- Debugging and Maintainance is easy.
---
+++ REACT/intro-to-react/with-react/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import { useState } from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+function App() {
+  const [value , setValue] = useState(0)
+  return (
+    <div className="App">
+      <Navbar/>
+      <div className="className">{value}</div>
+      <button onClick={()=>{setValue (value + 1)}}>click me</button>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
